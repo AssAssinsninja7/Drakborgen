3fefb06fcc852f4e34fba930e93349aa1cfde7ab
Jasmine Steen <jasmine.steen.wrk@gmail.com>
2019-04-03 20:23:37 +0200

    Added the AR session congig and a default AR camera to the project

---
+++ Assets/Scripts/SelectionMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionMenu_script : MonoBehaviour
+{
+    /*Add character_type
+      Add ring_type*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
