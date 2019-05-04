057b3bedd93dbc5ca393a27bbffb72300f1f86ad
maxwalhagen <maxwalhagen1@gmail.com>
2019-05-04 16:25:14 +0200

    Added roomScript and started fixing animation
    problem with animations so went back to maya and fixed stuff. need to fix on all tiles.

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class RoomScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Every tile will have this script
+
+    // Maybe move this enum to gameManager later
+    public enum RoomType { EventRoom, CorridorRoom, RotationRoom }
+    // public so we can set in editor 
+    public RoomType myRoomType;
+    int myRoomRotation = 0;
+    //                                   e1,     e2,   e3,     e4      public so we can set in editor 
+    public bool[] myExitDirections = { false, false, false, false };
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Init(int aRoomRotation)
     {
-        
+        if (aRoomRotation>360)
+        {
+            aRoomRotation -= 360;
+        }
+        myRoomRotation = aRoomRotation;
+    }
+    public bool[] GetExitDirections()
+    {
+        if (myRoomRotation > 0)
+        {
+            bool[] returnValue = myExitDirections;
+            if (myRoomRotation == 90)
+            {
+                // rotate it to new position
+                returnValue[0] = myExitDirections[3];
+                returnValue[1] = myExitDirections[0];
+                returnValue[2] = myExitDirections[1];
+                returnValue[3] = myExitDirections[2];
+                return returnValue;
+            }
+            else if (myRoomRotation == 180)
+            {
+                returnValue[0] = myExitDirections[2];
+                returnValue[1] = myExitDirections[3];
+                returnValue[2] = myExitDirections[0];
+                returnValue[3] = myExitDirections[1];
+                return returnValue;
+            }
+            else // == 270
+            {
+                returnValue[0] = myExitDirections[1];
+                returnValue[1] = myExitDirections[2];
+                returnValue[2] = myExitDirections[3];
+                returnValue[3] = myExitDirections[0];
+                return returnValue;
+            }
+        }
+
+        return myExitDirections;
+    }
+    public RoomType GetRoomType()
+    {
+        return myRoomType;
+    }
+
+    public int getRotation()
+    {
+        return myRoomRotation;
     }
 }
