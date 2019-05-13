5f9315cfeb1048bbca2e82669f0140761b847b61
Jasmine Steen <jasmine.steen.wrk@gmail.com>
2019-05-14 01:11:04 +0200

    The tiles are now loaded correctly

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -88,8 +88,14 @@ public class GameBoard : MonoBehaviour
     private GameObject e1TurnRoom;
 
     #endregion
-    // Start is called before the first frame update
 
+    //TestVariables
+    private bool isInit;
+
+    [SerializeField]
+    public Tilemap tileMap;
+
+    // Start is called before the first frame update
     void Start()
     {
         //Add start positions as listitems
@@ -99,12 +105,23 @@ public class GameBoard : MonoBehaviour
         startPositions.Add(new Vector2(9, 0));
         startPositions.Add(new Vector2(0, 6));
         startPositions.Add(new Vector2(9, 6));
+
+        roomStack = new Queue<GameObject>();
+
+        //TestVariable set
+        isInit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInit == false)
+        {
+            InitTestBoard();
+            isInit = true;
+        }
+
+
     }
 
     public void InitGameBoard(Player player1, Player player2) //take in startpos
@@ -131,7 +148,13 @@ public class GameBoard : MonoBehaviour
         //set startpos
     }
 
-
+    private void InitTestBoard()
+    {
+        //InitializeRoomTiles();
+        CreateBoardGraph();
+        LoadRoomStack();
+        ShuffleStack();
+    }
 
 
     public void GetNearestPointOnBoard(Vector3 hitPosition) //Select a tile
@@ -186,13 +209,15 @@ public class GameBoard : MonoBehaviour
     /// </summary>
     void CreateBoardGraph()
     {
-        for (int y = 0; y < 7; y++)
+        for (int y = tileMap.cellBounds.yMin; y < tileMap.cellBounds.yMax; y++)
         {
-            for (int x = 0; x < 10; x++)
+            for (int x = tileMap.cellBounds.xMin; x < tileMap.cellBounds.xMax; x++)
             {
-                boardMap[x, y].transform.position = new Vector3(transform.GetComponentInChildren<Tilemap>().transform.position.x * tileSize, 0, 
-                    transform.GetComponentInChildren<Tilemap>().transform.position.z * tileSize); //Maybe switch this to the y-pos
-                Debug.Log(x.ToString() + " " + y.ToString());
+                //boardMap[x, y].transform.position = new Vector3(transform.GetComponentInChildren<Tilemap>().transform.position.x * tileSize, 0, 
+                //    transform.GetComponentInChildren<Tilemap>().transform.position.z * tileSize); //Maybe switch this to the y-pos
+                //Vector3 localPos =
+                //boardMap[x, y].transform.position = tileMap.Cel
+                Debug.Log(boardMap[x,y].transform.position.ToString());
             }
         }
     }
@@ -205,68 +230,68 @@ public class GameBoard : MonoBehaviour
     void InitializeRoomTiles()
     {
         //EventRoom
-        Instantiate(e1nPnD); //deadend
-        e1nPnD.SetActive(false);
-        Instantiate(e1wPnD); //deadend with protcullis
-        e1wPnD.SetActive(false);
-        Instantiate(e12nPnD); //Left turn no portcullis
-        e12nPnD.SetActive(false);
-        Instantiate(e12wPnD); //Left turn with portcullis
-        e12wPnD.SetActive(false);
-        Instantiate(e13nPnD); //Straight forward
-        e13nPnD.SetActive(false);
-        Instantiate(e13wPwD3); //Door on the third entrance
-        e13wPwD3.SetActive(false);
-        Instantiate(e13wPnD); //Straight walk with portcullis
-        e13wPnD.SetActive(false);
-        Instantiate(e14nPnD); //Right turn
-        e14nPnD.SetActive(false);
-        Instantiate(e14wPnD); //Right turn with portcullis
-        e14wPnD.SetActive(false);
-        Instantiate(e124nPnD); //T-section left and right
-        e124nPnD.SetActive(false);
-        Instantiate(e124wPnD); //T-section left and right (with portcullis)
-        e124wPnD.SetActive(false);
-        Instantiate(e134nPnD); //T-section right and forward
-        e134nPnD.SetActive(false);
-        Instantiate(e123nPwD3); //T-section left and forward, door on e3
-        e123nPwD3.SetActive(false);
-        Instantiate(e134nPwD3); //T-section right and forward with door on e3
-        e134nPwD3.SetActive(false);
-        Instantiate(e1234nPwD3); //Crossroad with door on e3
-        e1234nPwD3.SetActive(false);
-        Instantiate(e1234nPwD234); //Crossroad with doors on e2, e3 & e4
-        e1234nPwD234.SetActive(false);
-        Instantiate(e1234Blocked); //Crossroad with block
-        e1234Blocked.SetActive(false);
-        Instantiate(e1234wPnD); //Crossroad with portcullis
-        e1234wPnD.SetActive(false);
-        Instantiate(e1234nPnD); //Crossroad
-        e1234nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1nPnD)); //deadend
+        //e1nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1wPnD)); //deadend with protcullis
+        //e1wPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e12nPnD)); //Left turn no portcullis
+        //e12nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e12wPnD)); //Left turn with portcullis
+        //e12wPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e13nPnD)); //Straight forward
+        //e13nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e13wPwD3)); //Door on the third entrance
+        //e13wPwD3.SetActive(false);
+        roomStack.Enqueue(Instantiate(e13wPnD)); //Straight walk with portcullis
+        //e13wPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e14nPnD)); //Right turn
+        //e14nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e14wPnD)); //Right turn with portcullis
+        //e14wPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e124nPnD)); //T-section left and right
+        //e124nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e124wPnD)); //T-section left and right (with portcullis)
+        //e124wPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e134nPnD)); //T-section right and forward
+        //e134nPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e123nPwD3)); //T-section left and forward, door on e3
+        //e123nPwD3.SetActive(false);
+        roomStack.Enqueue(Instantiate(e134nPwD3)); //T-section right and forward with door on e3
+        //e134nPwD3.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1234nPwD3)); //Crossroad with door on e3
+        //e1234nPwD3.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1234nPwD234)); //Crossroad with doors on e2, e3 & e4
+        //e1234nPwD234.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1234Blocked)); //Crossroad with block
+        //e1234Blocked.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1234wPnD)); //Crossroad with portcullis
+        //e1234wPnD.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1234nPnD)); //Crossroad
+        //e1234nPnD.SetActive(false);
 
 
 
 
 
         //CorridorRoom
-        Instantiate(e12Corridor); //Left turn
-        e12Corridor.SetActive(false);
-        Instantiate(e13Corridor); //Forward
-        e13Corridor.SetActive(false);
-        Instantiate(e14Corridor); //Right
-        e14Corridor.SetActive(false);
-        Instantiate(e123Corridor); //T-section to the left or forward
-        e123Corridor.SetActive(false);
-        Instantiate(e124Corridor); //T-section to the left and right
-        e124Corridor.SetActive(false);
-        Instantiate(e134Corridor); //T-section to the right or forward (from entrance)
-        e134Corridor.SetActive(false);
-        Instantiate(e1234Corridor);
-        e1234Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e12Corridor)); //Left turn
+        //e12Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e13Corridor)); //Forward
+        //e13Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e14Corridor)); //Right
+        //e14Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e123Corridor)); //T-section to the left or forward
+        //e123Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e124Corridor)); //T-section to the left and right
+        //e124Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e134Corridor)); //T-section to the right or forward (from entrance)
+        //e134Corridor.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1234Corridor));
+        //e1234Corridor.SetActive(false);
 
         //TurnRoom
-        Instantiate(e1TurnRoom);
-        e1TurnRoom.SetActive(false);
+        roomStack.Enqueue(Instantiate(e1TurnRoom));
+        //e1TurnRoom.SetActive(false);
 }
 
     /// <summary>
@@ -275,89 +300,89 @@ public class GameBoard : MonoBehaviour
     void LoadRoomStack()
     {
         //4st 4e no restriciton
-        roomStack.Enqueue(e1234nPnD);
-        roomStack.Enqueue(e1234nPnD);
-        roomStack.Enqueue(e1234nPnD);
-        roomStack.Enqueue(e1234nPnD);
+        roomStack.Enqueue(Instantiate(e1234nPnD));
+        roomStack.Enqueue(Instantiate(e1234nPnD));
+        roomStack.Enqueue(Instantiate(e1234nPnD));
+        roomStack.Enqueue(Instantiate(e1234nPnD));
         //2st 4e with door on e3
-        roomStack.Enqueue(e134nPwD3);
-        roomStack.Enqueue(e134nPwD3);
+        roomStack.Enqueue(Instantiate(e134nPwD3));
+        roomStack.Enqueue(Instantiate(e134nPwD3));
         //1st 4e with portcullis
-        roomStack.Enqueue(e1234wPnD);
+        roomStack.Enqueue(Instantiate(e1234wPnD));
         //1st 4e with doors on e1, e2 & e3
-        roomStack.Enqueue(e1234nPwD234);
+        roomStack.Enqueue(Instantiate(e1234nPwD234));
 
         //4st 3e with exits on e1, e2 & e3
-        roomStack.Enqueue(e134nPnD);
-        roomStack.Enqueue(e134nPnD);
-        roomStack.Enqueue(e134nPnD);
-        roomStack.Enqueue(e134nPnD);
+        roomStack.Enqueue(Instantiate(e134nPnD));
+        roomStack.Enqueue(Instantiate(e134nPnD));
+        roomStack.Enqueue(Instantiate(e134nPnD));
+        roomStack.Enqueue(Instantiate(e134nPnD));
         //1st 3e with door on e3
-        roomStack.Enqueue(e134nPwD3);
+        roomStack.Enqueue(Instantiate(e134nPwD3));
 
         //4st 3e with exits on e1, e2 & e3
         //Doesn't exist yet
         //1st 3e with door on e3
-        roomStack.Enqueue(e123nPwD3);
+        roomStack.Enqueue(Instantiate(e123nPwD3));
 
         //4st 3e exits on e1, e2 & e4
-        roomStack.Enqueue(e124nPnD);
-        roomStack.Enqueue(e124nPnD);
-        roomStack.Enqueue(e124nPnD);
-        roomStack.Enqueue(e124nPnD);
+        roomStack.Enqueue(Instantiate(e124nPnD));
+        roomStack.Enqueue(Instantiate(e124nPnD));
+        roomStack.Enqueue(Instantiate(e124nPnD));
+        roomStack.Enqueue(Instantiate(e124nPnD));
         //1st 2e with portcullis 
-        roomStack.Enqueue(e124wPnD);
+        roomStack.Enqueue(Instantiate(e124wPnD));
 
         //2st 2e exits on e1 & e2 (left turn)
-        roomStack.Enqueue(e12nPnD);
-        roomStack.Enqueue(e12nPnD);
+        roomStack.Enqueue(Instantiate(e12nPnD));
+        roomStack.Enqueue(Instantiate(e12nPnD));
         //1st 2e with portcullis
-        roomStack.Enqueue(e12wPnD);
+        roomStack.Enqueue(Instantiate(e12wPnD));
 
         //2st 2e exits on e1 and e4 (right turn)
-        roomStack.Enqueue(e14nPnD);
-        roomStack.Enqueue(e14nPnD);
+        roomStack.Enqueue(Instantiate(e14nPnD));
+        roomStack.Enqueue(Instantiate(e14nPnD));
         //1st 2e with portcullis
-        roomStack.Enqueue(e14wPnD);
+        roomStack.Enqueue(Instantiate(e14wPnD));
 
         //6st 2e exits on e1 and e3 (straight forward)
-        roomStack.Enqueue(e13nPnD);
-        roomStack.Enqueue(e13nPnD);
-        roomStack.Enqueue(e13nPnD);
-        roomStack.Enqueue(e13nPnD);
-        roomStack.Enqueue(e13nPnD);
-        roomStack.Enqueue(e13nPnD);
+        roomStack.Enqueue(Instantiate(e13nPnD));
+        roomStack.Enqueue(Instantiate(e13nPnD));
+        roomStack.Enqueue(Instantiate(e13nPnD));
+        roomStack.Enqueue(Instantiate(e13nPnD));
+        roomStack.Enqueue(Instantiate(e13nPnD));
+        roomStack.Enqueue(Instantiate(e13nPnD));
         //1st 2e with portcullis 
-        roomStack.Enqueue(e13wPnD);
+        roomStack.Enqueue(Instantiate(e13wPnD));
         //1st 2e with portcullis and door on e3
-        roomStack.Enqueue(e13wPwD3);
+        roomStack.Enqueue(Instantiate(e13wPwD3));
 
         //2st 1e (Deadend)
-        roomStack.Enqueue(e1nPnD);
-        roomStack.Enqueue(e1nPnD);
+        roomStack.Enqueue(Instantiate(e1nPnD));
+        roomStack.Enqueue(Instantiate(e1nPnD));
         //1st 1e with portcullis
-        roomStack.Enqueue(e1wPnD);
+        roomStack.Enqueue(Instantiate(e1wPnD));
 
         //3st turnRooms
-        roomStack.Enqueue(e1TurnRoom);
-        roomStack.Enqueue(e1TurnRoom);
-        roomStack.Enqueue(e1TurnRoom);
+        roomStack.Enqueue(Instantiate(e1TurnRoom));
+        roomStack.Enqueue(Instantiate(e1TurnRoom));
+        roomStack.Enqueue(Instantiate(e1TurnRoom));
 
         //1st 4e Corridor
-        roomStack.Enqueue(e1234Corridor);
+        roomStack.Enqueue(Instantiate(e1234Corridor));
         //1st 2e exits on e1 & e2, Corridor (left turn)
-        roomStack.Enqueue(e12Corridor);
+        roomStack.Enqueue(Instantiate(e12Corridor));
         //1st 2e exits on e1, & e4, Corridor (right turn)
-        roomStack.Enqueue(e14Corridor);
+        roomStack.Enqueue(Instantiate(e14Corridor));
         //1st 2e exits on e1 & e3, Corridor (straight forward
-        roomStack.Enqueue(e13Corridor);
+        roomStack.Enqueue(Instantiate(e13Corridor));
 
         //1st 3e exits on e1, e2, & e3. Corridor
-        roomStack.Enqueue(e123Corridor);
+        roomStack.Enqueue(Instantiate(e123Corridor));
         //1st 3e exits on e1, e2 & e4. Corridor
-        roomStack.Enqueue(e124Corridor);
+        roomStack.Enqueue(Instantiate(e124Corridor));
         //1st 3e exits on e1, e3, e4
-        roomStack.Enqueue(e134Corridor);
+        roomStack.Enqueue(Instantiate(e134Corridor));
     }
 
     /// <summary>
